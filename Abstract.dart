844d67a8a1b9607e86c0c29f2e209fc235cf6104
sid <siddhantpalde@gmail.com>
2020-07-16 00:14:10 +0530

    My first commit

---
+++ Abstract.dart
@@ -0,0 +1,25 @@
+void main() {
+  Shape s1 = new Rectangle();
+  s1.draw();
+  Shape s2 = new Circle();
+  s2.draw();
+}
+abstract class Shape {
+   void draw() {
+     print("Drawing shape");
+   }
+}
+class Rectangle extends Shape {
+//   void draw() {
+//     print("Drawing Rectangle .....");      
+//   }
+
+}
+class Circle extends Shape {
+  void draw() {
+    print("Drawing Circle .....");
+  }
+}
+class Triangle {
+  
+}
